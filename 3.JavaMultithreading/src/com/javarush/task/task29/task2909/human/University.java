9aa8b2427801c50b11b49ead5a52a60f908802e9
J Zim <zimjavajob@gmail.com>
2017-03-20 17:02:54 +0500

    create class UniversityPerson and refactoring  class Teacher, Student, University. Complete refactoring(4).

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task29.task2909.human;
 
-public class University extends Student {
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
 
     public University(String name, int age) {
-        super(name, age, 0);
+        this.name = name;
+        this.age = age;
     }
 
     public Student getStudentWithAverageGrade() {
@@ -19,4 +23,32 @@ public class University extends Student {
     public void getStudentWithMinAverageGradeAndExpel() {
         //TODO:
     }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private List<Student> students = new ArrayList<>();
+    private String name;
+    private int age;
 }
