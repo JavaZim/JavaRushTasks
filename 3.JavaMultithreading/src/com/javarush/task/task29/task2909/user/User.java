8626435c08a5cab1d6fa081ee8c0d602dd704c20
J Zim <zimjavajob@gmail.com>
2017-03-21 16:41:06 +0500

    complete refactoring.

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,8 @@ public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
+    private boolean man;
+    private Address address;
 
     private Work work;
 
@@ -41,24 +39,9 @@ public class User {
         this.age = age;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -67,6 +50,38 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public String getCountry() {
+        return address.getCountry();
+    }
+
+    public void setCountry(String country) {
+        address.setCountry(country);
+    }
+
+    public String getCity() {
+        return address.getCity();
+    }
+
+    public void setCity(String city) {
+        address.setCity(city);
+    }
+
+    public void setHouse(String house) {
+        address.setHouse(house);
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public String getBoss(){
+        return work.getBoss();
     }
 
     public void printInfo(){
