0aee531a13b6200a65d27a553ee4accfe94c6a39
J Zim <zimjavajob@gmail.com>
2017-03-22 14:02:53 +0500

    create class CustommizeComparable and implement Comparable<T>

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task26.task2603;
+
+import java.util.Comparator;
+
+/*
+Убежденному убеждать других не трудно
+*/
+public class Solution {
+
+    public static CustomizedComparator comparator;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class CustomizedComparator<T> implements Comparator<T>{
+
+
+        public CustomizedComparator(Comparator<T> ... vararg){
+            this.comparators = vararg;
+        }
+
+        @Override
+        public int compare(T o1, T o2) {
+
+            int result = 0;
+
+            for (Comparator comporator :comparators) {
+
+                result =  comporator.compare(o1, o2);
+                if(result != 0){
+                    break;
+                }
+
+            }
+            return result;
+
+        }
+
+        private Comparator<T> [] comparators;
+    }
+}
