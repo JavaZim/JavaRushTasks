0372776585fe0c560c017003f5e0003141129bb0
J Zim <zimjavajob@gmail.com>
2017-03-23 16:45:08 +0500

    release class Server

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,37 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args) {
 
+        int port = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Сервер запущен");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static class Handler extends Thread {
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        private Socket socket;
     }
 
 }
