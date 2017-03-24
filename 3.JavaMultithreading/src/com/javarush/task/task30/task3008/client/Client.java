8b55c51804422eceffa28db42feecdfa26969d75
J Zim <zimjavajob@gmail.com>
2017-03-24 15:20:07 +0500

    complete 3008(13)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,14 +1,49 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by jdk on 24.03.2017.
  */
 public class Client {
 
+    protected String getServerAddress() {
+        return ConsoleHelper.readString();
+    }
 
-    public class SocketThread extends Thread{
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            clientConnected = false;
+            ConsoleHelper.writeMessage("Message not sent");
+        }
+    }
+
+
+
+    public class SocketThread extends Thread {
 
     }
 
