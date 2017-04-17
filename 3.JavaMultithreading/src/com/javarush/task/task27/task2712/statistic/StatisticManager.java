61ccb1b4ac38b683affe8b28b628b46468821728
J Zim <zimjavajob@gmail.com>
2017-04-17 10:35:38 +0500

    Restaurant 12

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
 
@@ -15,5 +21,17 @@ public class StatisticManager {
     private StatisticManager() {
     }
 
+    private class StatisticStorage{
+
+        public StatisticStorage() {
+            for (EventType event : EventType.values()) {
+                storage.put(event, new ArrayList<>());
+            }
+        }
+
+        Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+    }
+
     private static StatisticManager ourInstance = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
 }
